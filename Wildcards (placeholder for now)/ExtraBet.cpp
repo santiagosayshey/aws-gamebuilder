65ac60f39f5af01dc4ce5fbc72e38b1390e988b6
Gregorius B <gregoriusbaswarawira.nuraga@student.adelaide.edu.au>
2025-01-03 12:58:26 +1030

    feat(wildcards/ExtraBet) : implement the logic for ExtraBet (need to have functions to get player's money) - Commented out how it works until we have the player's function - double the player money

--- Wildcards (placeholder for now)/ExtraBet.cpp
+++ Wildcards (placeholder for now)/ExtraBet.cpp
@@ -1,1 +1,29 @@
+#include "Wildcards.hpp"
+
+
+class ExtraBet : public Wildcard {
+public:
+// need a player class that has current money function
+    void activate(Player& player, std::vector<std::shared_ptr<Player>>& players, Deck& deck) override {
+        //double currentMoney = player.getCurrentMoney();
+        //player.setCurrentMoney(currentMoney * 2);
+        //std::cout << "Extra Bet activated: Player's money doubled to " << player.getCurrentMoney() << std::endl;
+    }
+
+    std::string description() const override {
+        return "Player's money doubled";
+    }
+};
+
+// Factory method 
+std::shared_ptr<Wildcard> Wildcard::createWildcard(WildcardType type) {
+    if (type == WildcardType::ExtraBet) {
+        return std::make_shared<ExtraBet>();
+    }
+    return nullptr;
+}
+
+// make sure Player class has 
+// the getCurrentMoney() and setCurrentMoney()
+
 
