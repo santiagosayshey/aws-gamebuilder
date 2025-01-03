253baf81a98ff26fb34ea5f8a45230e0af6da498
Gregorius B <gregoriusbaswarawira.nuraga@student.adelaide.edu.au>
2025-01-03 12:46:52 +1030

    fix(wildcards): Added factory method to create instances of wildcard in the .hpp file - specified all the wildcards on the hpp files - create placeholder files for the wildcards I'm working on

--- Wildcards (placeholder for now)/Wildcards.hpp
+++ Wildcards (placeholder for now)/Wildcards.hpp
@@ -5,14 +5,32 @@
 #include <memory>
 //#include "/entity/player.hpp"
 //#include "/entity/card.hpp"
+#include "states/GameState.hpp"
 
 
 using namespace std;
+
+// specify the wildcard types
+enum class WildcardType{
+    Swap,
+    Foresight,
+    SeeThrough,
+    Duplicate,
+    DeckPeek,
+    ExtraBet,
+    SafeHit,
+    InstantStand,
+    Freeze,
+    SecondChance
+};
 
 class Wildcard{
     public:
         virtual ~Wildcard() = default;
         // the wildcard logic
         virtual void activate(Player& player, std::vector<std::shared_ptr<Player>>& players, Deck& deck) = 0;
+        // description
         virtual std::string description() const = 0;
+        // Factory method to create instance of wildcard
+        static std::shared_ptr<Wildcard> createWildcard(WildcardType type);
 };
