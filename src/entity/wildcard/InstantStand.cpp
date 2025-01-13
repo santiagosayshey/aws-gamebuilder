589a6ee116b6e34d529f55359370fd29c440f5fa
Sam Chau <chaucted@gmail.com>
2025-01-13 10:42:57 +1030

    feat: update InstantStand to add a card with value 20 for special gameplay effect

--- src/entity/wildcard/InstantStand.cpp
+++ src/entity/wildcard/InstantStand.cpp
@@ -11,7 +11,7 @@ void InstantStand::use(Player &owner, std::vector<Player> &allPlayers) {
     // Hacky approach: create a "value=20" card with some arbitrary suit
     // so the player's "calculateHandTotal()" sees 20. 
     // NOTE: 20 is outside normal 1-13 range, but that's presumably the effect you want:
-    owner.addCard(std::make_shared<Card>(13, Suit::Spades)); 
+    owner.addCard(std::make_shared<Card>(20, Suit::Spades)); 
 }
 
 std::string InstantStand::getName() const {
